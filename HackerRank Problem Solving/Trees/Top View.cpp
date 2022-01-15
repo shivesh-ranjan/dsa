1da2ade65776efde0a9b6ef92fa1d339a6d27c54
shivesh-ranjan <ranjanshivesh71@gmail.com>
2022-01-15 05:47:42 +0000

    top view

---
+++ HackerRank Problem Solving/Trees/Top View.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+    void topView(Node * root){
+        vector<int> ans;
+        if(root==NULL) return;
+        map<int,int> mpp;
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            Node* node=it.first;
+            int line=it.second;
+            if(mpp.find(line)==mpp.end()) mpp[line]=node->data;
+            if(node->left!=NULL) q.push({node->left,line-1});
+            if(node->right!=NULL) q.push({node->right,line+1});
+        }
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        for(auto it:ans){
+            printf("%d ",it);
+        }
+    }
+
+}; 
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.topView(root);
+
+    return 0;
+}
